57e7d831309aa6fbd5b5c086ca29751106dbdd43
Filip Patyk <fp.patyk@gmail.com>
2024-05-30 22:05:19 +0200

    docs: adding docstrings for trackers module

--- src/trackers/callbacks.py
+++ src/trackers/callbacks.py
@@ -1,22 +1,35 @@
 import torch
-from lightning import Callback
-
 import wandb
+from lightning import Callback
 
 
 class ImagePredictionLogger(Callback):
-    def __init__(self, val_samples, num_samples=32):
+    """
+    Callback for logging image predictions during validation.
+
+    Args:
+        val_samples (tuple): A tuple containing validation images and labels.
+        num_samples (int): Number of samples to log. Default is 32.
+    """
+
+    def __init__(self, val_samples: tuple, num_samples: int = 32):
         super().__init__()
         self.num_samples = num_samples
         self.val_imgs, self.val_labels = val_samples
 
     def on_validation_epoch_end(self, trainer, pl_module):
-        # Bring the tensors to CPU
+        """
+        Method called at the end of each validation epoch.
+
+        Args:
+            trainer (Trainer): The PyTorch Lightning Trainer object.
+            pl_module (LightningModule): The PyTorch Lightning module being trained.
+        """
         val_imgs = self.val_imgs.to(device=pl_module.device)
         val_labels = self.val_labels.to(device=pl_module.device)
-        # Get model prediction
         logits = pl_module(val_imgs)
         preds = torch.argmax(logits, -1)
+
         # Log the images as wandb Image
         trainer.logger.experiment.log(
             {

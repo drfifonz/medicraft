071e0347b7d6c0d27b5fa8dae33c035bcbfd94cf
Filip Patyk <fp.patyk@gmail.com>
2025-02-08 22:11:29 +0100

    chore: add generating step job

---
+++ cluster/run-generate-stepped.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=medicraft-generating-stepped
+#SBATCH --partition=gpu_spot
+#SBATCH --array=1-4
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=1:00:00:00
+#SBATCH --output=.run-logs/stepped/run-%A-%a.out
+#SBATCH --mail-user=fp.patyk@gmail.com
+#SBATCH --mail-type=ALL
+
+
+source /home/fpatyk/.cache/pypoetry/virtualenvs/medicraft-7W8s1oxG-py3.11/bin/activate
+
+
+
+cd /work/$USER/medicraft || exit -1
+
+
+srun python src/medicraft/main.py  -f configs/generate-stepped.yml --step $SLURM_ARRAY_TASK_ID
+# srun python src/medicraft/main.py  -f configs/train-generator.yml --step $SLURM_ARRAY_TASK_ID
+
+
+
+
